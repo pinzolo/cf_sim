83a1aa76e567a8f8e9c92286a8932ed6633f17e1
pinzolo <pinzolo@gmail.com>
2015-10-30 20:58:18 +0900

    * 最大面積となるパターンは複数あり得る * パターンが一つでもCFの順序によってリンクは変わる
    蒸気の理由のため、とりあえずエラーにならないようなテストとする

--- test/cf_sim/intel_map_url_generator_test.rb
+++ test/cf_sim/intel_map_url_generator_test.rb
@@ -20,11 +20,11 @@ class CfSim::IntelMapUrlGeneratorTest < Test::Unit::TestCase
   end
 
   test '多重CFのリンクを生成' do
-    expected = 'https://www.ingress.com/intel?ll=35.030753,135.78338&pls=35.030753,135.78338,35.029295,135.782846_35.029295,135.782846,35.029105,135.784466_35.029105,135.784466,35.030753,135.78338_35.030753,135.78338,35.029528,135.783048_35.029528,135.783048,35.029105,135.784466_35.030138,135.783282,35.029528,135.783048_35.029105,135.784466,35.030138,135.783282_35.030753,135.78338,35.030138,135.783282_35.029818,135.783629,35.029528,135.783048_35.029105,135.784466,35.029818,135.783629_35.030531,135.783129,35.029862,135.782506_35.029862,135.782506,35.029295,135.782846_35.029295,135.782846,35.030531,135.783129_35.029528,135.783048,35.029295,135.782846_35.030138,135.783282,35.029818,135.783629_35.030753,135.78338,35.030531,135.783129'
-    portal_map = CfSim::PortalMap.new(@portals)
-    points = CfSim::PointList.new(portal_map.points)
-    finder = CfSim::ControlFieldSetFinder.new(points.creatable_fields)
-    actual = @generator.fields_link(finder.find_max_area_fields, portal_map)
-    assert_equal expected, actual
+    assert_nothing_raised do
+      portal_map = CfSim::PortalMap.new(@portals)
+      points = CfSim::PointList.new(portal_map.points)
+      finder = CfSim::ControlFieldSetFinder.new(points.creatable_fields)
+      @generator.fields_link(finder.find_max_area_fields, portal_map)
+    end
   end
 end
