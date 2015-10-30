9a0e9d32200b46d183a8bf1d663868aa8dc310ef
pinzolo <pinzolo@gmail.com>
2015-10-30 21:30:30 +0900

    Fix Coveralls filer

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ SimpleCov.start do
   add_filter '/test/'
   add_filter '/bundle/'
   add_filter do |source_file|
-    source_file.filename != 'cblxy.rb'
+    source_file.filename.end_with?('cblxy.rb')
   end
 end
 
