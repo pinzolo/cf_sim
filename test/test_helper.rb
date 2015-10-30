a338d7da5e0644c663922d4a03d7e64230f47b5a
pinzolo <pinzolo@gmail.com>
2015-10-30 21:17:14 +0900

    Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,10 @@
+require 'coveralls'
+require 'simplecov'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/bundle/'
+end
+
 require 'test/unit'
 require 'cf_sim'
