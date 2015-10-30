817583a4ae07160edb5b8d075b565e84b9e1b15d
pinzolo <pinzolo@gmail.com>
2015-10-30 21:27:51 +0900

    Fix Coveralls filter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,9 @@ SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter '/test/'
   add_filter '/bundle/'
-  add_filter '/cblxy/'
+  add_filter do |source_file|
+    source_file.filename != 'cblxy.rb'
+  end
 end
 
 require 'test/unit'
